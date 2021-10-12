ab5b7fdd5d1542921b2dc790dc0efb35f00923de
Shubham Mishra <mey207647@mech.iitd.ac.in>
2021-10-12 11:20:20 +0530

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,14 @@
+import random
+def guess(x):
+    comp_guess=random.randint(1,x)
+    while True:
+        your_guess= int(input(f'Put your number (between 1 and {x}) here:'))
+        if your_guess<comp_guess:
+            print(f'Guess higher than {your_guess}')
+        elif your_guess>comp_guess:
+            print(f'Guess lower than {your_guess}')
+        else:
+            print(f'You guessed it right, the number is indeed {comp_guess}')
+
+
+guess(20)
